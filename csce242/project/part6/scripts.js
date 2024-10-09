5d11ae74f46f0db9c54d196b87c2e76c00a70e6e
caelen wall <cswall@email.sc.edu>
2024-10-09 18:24:13 -0400

    update

---
+++ csce242/project/part6/scripts.js
@@ -0,0 +1,67 @@
+
+
+document.querySelector("#toggle-nav").onclick = () => {
+    document.getElementById("nav-items").classList.toggle("hidden-small");
+};
+document.getElementById('toggle').addEventListener('change', function() {
+    document.body.classList.toggle('dark-mode', this.checked);
+});
+
+const getSpending = async() => {
+    const url = "https://caelenw.github.io/csce242/project/part6/example.json";
+
+    try {
+        const response = await fetch(url);
+        return response.json();
+    } catch(error){
+        console.log(error)
+    }
+};
+
+const showSpending = async() => {
+    const spending = await getSpending();
+    
+    spending.forEach((spendingItem) => {
+        document.getElementById("spending-section").append(getSpendingSection(spendingItem));
+    });
+};
+
+const getSpendingSection = (spendingItem) => {
+    const section = document.createElement("section");
+
+    const logo = document.createElement("img");
+    logo.innerHTML = spendingItem.logo;
+    section.append(logo);
+
+    const h3 = document.createElement("h3");  // Added quotes here
+    h3.innerHTML = spendingItem.Item;  // Corrected the property access
+    section.append(h3);
+
+    const priceP = document.createElement("p"); // Fixed variable name
+    priceP.innerHTML = spendingItem.Price;
+    section.append(priceP); // Corrected to append priceP
+    
+    const accountP = document.createElement("p");
+    accountP.innerHTML = spendingItem.Account;
+    section.append(accountP);
+
+    const dateP = document.createElement("p");
+    dateP.innerHTML = spendingItem.Date;
+    section.append(dateP);
+
+    const catP = document.createElement("p");
+    catP.innerHTML= spendingItem.Categorie;
+    section.append(catP);
+
+    const statusP = document.createElement("p");
+    statusP.innerHTML = spendingItem.Status;
+    section.append(statusP);
+
+    const commentP = document.createElement("p");
+    commentP.innerHTML= spendingItem.Comments;
+    section.append(commentP);
+
+    return section; // Don't forget to return the section
+};
+
+showSpending();

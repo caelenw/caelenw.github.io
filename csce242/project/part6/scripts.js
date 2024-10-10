421dff83a6240d77622e32b329c9c474af1f12c2
caelen wall <cswall@email.sc.edu>
2024-10-10 01:23:29 -0400

    update

--- csce242/project/part6/scripts.js
+++ csce242/project/part6/scripts.js
@@ -1,26 +1,25 @@
-
-
 document.querySelector("#toggle-nav").onclick = () => {
     document.getElementById("nav-items").classList.toggle("hidden-small");
 };
+
 document.getElementById('toggle').addEventListener('change', function() {
     document.body.classList.toggle('dark-mode', this.checked);
 });
 
-const getSpending = async() => {
+const getSpending = async () => {
     const url = "https://caelenw.github.io/csce242/project/part6/example.json";
 
     try {
         const response = await fetch(url);
         return response.json();
-    } catch(error){
-        console.log(error)
+    } catch (error) {
+        console.log(error);
     }
 };
 
-const showSpending = async() => {
+const showSpending = async () => {
     const spending = await getSpending();
-    
+
     spending.forEach((spendingItem) => {
         document.getElementById("spending-section").append(getSpendingSection(spendingItem));
     });
@@ -28,41 +27,43 @@ const showSpending = async() => {
 
 const getSpendingSection = (spendingItem) => {
     const section = document.createElement("section");
-
     const logo = document.createElement("img");
-    logo.innerHTML = spendingItem.logo;
+    logo.src = spendingItem.logo; 
+ 
     section.append(logo);
 
-
-    const h3 = document.createElement("h3");  
-    h3.innerHTML = spendingItem.Item;  
+    document.body.append(section);
+    const h3 = document.createElement("h3");
+    h3.innerHTML = spendingItem.Item;
     section.append(h3);
 
-    const priceP = document.createElement("p"); 
-    priceP.innerHTML = spendingItem.Price;
-    section.append(priceP); 
-    
+    const priceP = document.createElement("p");
+    priceP.innerHTML = "Price: " + spendingItem.Price;
+    section.append(priceP);
+
     const accountP = document.createElement("p");
-    accountP.innerHTML = spendingItem.Account;
+    accountP.innerHTML = "Account: " + spendingItem.Account;
     section.append(accountP);
 
     const dateP = document.createElement("p");
-    dateP.innerHTML = spendingItem.Date;
+    dateP.innerHTML = "Date: " + spendingItem.Date;
     section.append(dateP);
 
     const catP = document.createElement("p");
-    catP.innerHTML= spendingItem.Categorie;
+    catP.innerHTML = "Category: " + spendingItem.Categorie;
     section.append(catP);
 
     const statusP = document.createElement("p");
-    statusP.innerHTML = spendingItem.Status;
+    statusP.innerHTML = "Status: " + spendingItem.Status;
     section.append(statusP);
 
     const commentP = document.createElement("p");
-    commentP.innerHTML= spendingItem.Comments;
+    commentP.innerHTML = "Comments: " + spendingItem.Comments;
     section.append(commentP);
 
-    return section; 
+
+
+    return section;
 };
 
 showSpending();
